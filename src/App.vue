71bd3b2a9df19310f6b9aea5061cc2789d26c992
amumu <1745627971@qq.com>
2023-07-25 23:51:19 +0800

    初次提交

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">111</div>
+</template>
+<script setup lang="ts">
+console.log('wosini')
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+.container {
+  width: 100px;
+  height: 100px;
+  background-color: red;
+}
+</style>
