961fc950287f71d5f87e43d67d3fed6d2d93f558
aaa150010 <102653995+aaa150010@users.noreply.github.com>
2023-07-26 17:05:43 +0800

    fix: 新增

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
-  <div></div>
+  <el-pagination
+    :page-sizes="[100, 200, 300, 400]"
+    layout="total, sizes, prev, pager, next, jumper"
+    :total="400"
+  />
+  <svg>
+    <use xlink:href="#icon-phone" fill="red"></use>
+  </svg>
+  引入svg
 </template>
 <script setup lang="ts"></script>
 <style>
 * {
   margin: 0;
   padding: 0;
-}
-.container {
-  width: 100px;
-  height: 100px;
-  background-color: red;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-
+import App from '@/App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+//引入svg
+import 'virtual:svg-icons-register'
 const app = createApp(App)
+app.use(ElementPlus, {
+  //注册成中文
+  locale: zhCn,
+})
+console.log(import.meta.env)
 // app.use(router)
 app.mount('#app')
-const fun = () => {
-  console.log('你好')
-}
-fun()
