3434a7732662bb7728d8d6c8f137d0a9095fe6dc
aaa150010 <102653995+aaa150010@users.noreply.github.com>
2023-07-28 14:11:07 +0800

    fix: 新增

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,38 @@
-import { defineConfig } from 'vite'
+// https://vitejs.dev/config/
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+//引入svg需要用到插件
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    createSvgIconsPlugin({
-      // Specify the icon folder to be cached
-      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
-      // Specify symbolId format
-      symbolId: 'icon-[dir]-[name]',
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve('./src'), // 相对路径别名配置，使用 @ 代替 src
+export default defineConfig(({ mode }) => {
+  //获取各种环境下的对应的变量
+  const env = loadEnv(mode, process.cwd())
+  return {
+    plugins: [
+      vue(),
+      createSvgIconsPlugin({
+        iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+        symbolId: 'icon-[dir]-[name]',
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'), // 相对路径别名配置，使用 @ 代替 src
+      },
     },
-  },
+    //代理跨域
+    server: {
+      proxy: {
+        [env.VITE_APP_BASE_API]: {
+          //获取数据的服务器地址设置
+          target: env.VITE_SERVE,
+          //需要代理跨域
+          changeOrigin: true,
+          //路径重写
+          //开发环境处理跨域，生产和测试环境不用处理  交给后端
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
+  }
 })
